dd0feacc090983809730ca9a7eba910d044717b4
Grigory Kisialev <grigory_kisialev@epam.com>
2019-12-01 14:48:28 +0300

    step13

--- KatlaSport.Services.Models/HiveManagement/IHiveSectionService.cs
+++ KatlaSport.Services.Models/HiveManagement/IHiveSectionService.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 namespace KatlaSport.Services.HiveManagement
 {
     /// <summary>
-    /// Represents a hive section service.
+    /// Represents service of a hive section.
     /// </summary>
     public interface IHiveSectionService
     {
@@ -15,25 +15,47 @@ namespace KatlaSport.Services.HiveManagement
         Task<List<HiveSectionListItem>> GetHiveSectionsAsync();
 
         /// <summary>
-        /// Gets a hive section.
+        /// Gets the section of hive.
         /// </summary>
         /// <param name="id">A hive section identifier.</param>
         /// <returns>A <see cref="Task{HiveSection}"/>.</returns>
         Task<HiveSection> GetHiveSectionAsync(int id);
 
         /// <summary>
-        /// Gets a list of hive sections for specified hive.
+        /// Task lists hive sections for a concrete hive.
         /// </summary>
         /// <param name="hiveId">A hive identifier.</param>
         /// <returns>A <see cref="Task{List{HiveSectionListItem}}"/>.</returns>
         Task<List<HiveSectionListItem>> GetHiveSectionsAsync(int hiveId);
 
         /// <summary>
-        /// Sets deleted status for a hive section.
+        /// Task updates a hive section.
+        /// </summary>
+        /// <param name="hiveSectionId">A hive section id.</param>
+        /// <param name="updateRequest">An update hive section.</param>
+        /// <returns>A <see cref="Task{HiveSection}"/>.</returns>
+        Task<HiveSection> UpdateHiveSectionAsync(int hiveSectionId, UpdateHiveSectionRequest updateRequest);
+
+        /// <summary>
+        /// Task sets deleted status for a hive section.
         /// </summary>
         /// <param name="hiveSectionId">A hive section identifier.</param>
         /// <param name="deletedStatus">Status.</param>
         /// <returns>A <see cref="Task"/>.</returns>
         Task SetStatusAsync(int hiveSectionId, bool deletedStatus);
+
+        /// <summary>
+        /// Task creates a hive section.
+        /// </summary>
+        /// <param name="createRequest">A new hive section.</param>
+        /// <returns>A <see cref="Task{HiveSection}"/>.</returns>
+        Task<HiveSection> CreateHiveSectionAsync(UpdateHiveSectionRequest createRequest);
+
+        /// <summary>
+        /// Task deletes a hive section.
+        /// </summary>
+        /// <param name="hiveSectionId">A hive section id.</param>
+        /// <returns>A <see cref="Task{HiveSection}"/>.</returns>
+        Task DeleteHiveSectionAsync(int hiveSectionId);
     }
 }
--- KatlaSport.Services.Models/HiveManagement/UpdateHiveSectionRequestValidator.cs
+++ KatlaSport.Services.Models/HiveManagement/UpdateHiveSectionRequestValidator.cs
@@ -1,9 +1,11 @@
-﻿namespace KatlaSport.Services.HiveManagement
+﻿using FluentValidation;
+
+namespace KatlaSport.Services.HiveManagement
 {
     /// <summary>
     /// Represents a validator for <see cref="UpdateHiveSectionRequestValidator"/>
     /// </summary>
-    /*public class UpdateHiveSectionRequestValidator : AbstractValidator<UpdateHiveSectionRequest>
+    public class UpdateHiveSectionRequestValidator : AbstractValidator<UpdateHiveSectionRequest>
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="UpdateHiveSectionRequestValidator"/> class.
@@ -13,5 +15,5 @@
             RuleFor(r => r.Name).Length(4, 60);
             RuleFor(r => r.Code).Length(5);
         }
-    }*/
+    }
 }
