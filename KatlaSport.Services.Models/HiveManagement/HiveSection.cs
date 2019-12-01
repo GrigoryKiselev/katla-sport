d6d792a160e66f89c57c693b4ac66437896c9fa3
Grigory Kisialev <grigory_kisialev@epam.com>
2019-12-01 19:21:00 +0300

    step14

--- KatlaSport.Services.Models/HiveManagement/HiveSection.cs
+++ KatlaSport.Services.Models/HiveManagement/HiveSection.cs
@@ -8,6 +8,26 @@ namespace KatlaSport.Services.HiveManagement
     public class HiveSection : HiveSectionListItem
     {
         /// <summary>
+        /// Gets or sets a hive section identifier.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets a hive setion name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets a hive section code.
+        /// </summary>
+        public string Code { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether a hive section is deleted.
+        /// </summary>
+        public bool IsDeleted { get; set; }
+
+        /// <summary>
         /// Gets or sets a timestamp when the hive section was updated last time.
         /// </summary>
         public DateTime LastUpdated { get; set; }
--- KatlaSportNg/src/app/hive-management/forms/hive-section-form.component.ts
+++ KatlaSportNg/src/app/hive-management/forms/hive-section-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HiveSection } from '../models/hive-section';
+import { HiveSectionService } from '../services/hive-section.service';
 
 @Component({
   selector: 'app-hive-section-form',
@@ -6,9 +9,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hive-section-form.component.css']
 })
 export class HiveSectionFormComponent implements OnInit {
+ public hiveSection = new HiveSection(0, "", "", 0, false, "");
+  public existed = false;
 
-  constructor() { }
+   constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private hiveSectionService: HiveSectionService
+  ) { }
 
-  ngOnInit() {
+   ngOnInit() : void {
+    this.route.params.subscribe(p => {
+      if (p['id'] === undefined) {
+        this.hiveSection.storeHiveId = p['hiveId'];
+        return;
+      }
+       this.hiveSectionService.getHiveSection(p['id']).subscribe(h => {
+         this.hiveSection = h;
+        this.hiveSection.storeHiveId = p['hiveId'];
+       });
+      this.existed = true;
+    });  
+  }
+
+   navigateToHiveSections() : void {
+    this.router.navigate([`/hive/${this.hiveSection.storeHiveId}/sections`]);
+  }
+
+   onCancel() : void {
+    this.navigateToHiveSections();
+  }
+
+   onSubmit() : void {
+    if (this.existed) {
+      this.hiveSectionService.updateHiveSection(this.hiveSection).subscribe(p => this.navigateToHiveSections());
+    } else {
+      this.hiveSectionService.addHiveSection(this.hiveSection).subscribe(p => this.navigateToHiveSections());
+    }
+  }
+
+   onDelete() : void {
+    this.hiveSectionService.setHiveSectionStatus(this.hiveSection.id, true).subscribe(s => this.hiveSection.isDeleted = true);
+  }
+
+   onUndelete() : void {
+    this.hiveSectionService.setHiveSectionStatus(this.hiveSection.id, false).subscribe(s => this.hiveSection.isDeleted = false);
+  }
+
+   onPurge() : void {
+    this.hiveSectionService.deleteHiveSection(this.hiveSection.id).subscribe(s => this.navigateToHiveSections());
   }
 }
